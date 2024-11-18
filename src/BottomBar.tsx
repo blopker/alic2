dad6f0a5577356a9daa3bfe2259d1ac650f63965
Bo Lopker <lopkerk@gmail.com>
2024-11-18 18:53:32 +0700

    button

--- src/BottomBar.tsx
+++ src/BottomBar.tsx
@@ -3,8 +3,20 @@
 
 export default function BottomBar() {
   return (
-    <div class="fixed bottom-0 left-0 right-0 flex items-center justify-center bg-gray-400 h-10 px-4 shadow-2xl">
-      hi
+    <div class="fixed bottom-0 left-0 right-0 flex items-center justify-between bg-gray-400 h-8 px-2 shadow-2xl">
+      <AddButton />
+      <div>hi</div>
     </div>
+  );
+}
+
+function AddButton() {
+  return (
+    <button
+      type="button"
+      class="relative border-[0.5px] rounded h-5 w-8 hover:bg-gray-300 p-0 m-0 leading-none"
+    >
+      +
+    </button>
   );
 }
